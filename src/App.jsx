d572749ad156ca16e18f5ff397f9035836336cae
pr-at-ham <prathampotadar143@gmail.com>
2024-08-22 15:57:21 +0530

    frontend starter pack

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import {motion} from "framer-motion"
+import { useState } from "react";
 function App() {
+  const [change,setChange] = useState(true)
+  let str = "flex justify-center items-center h-lvh"
+  let str1 = "flex justify-center items-center h-lvh bg-gray-300"
   return (
-    <>
-      <div className="flex w-full justify-center items-center h-screen">
-        <p className="text-2xl font-bold">
-          Thid Project is created using Vite + React + Tailwind CSS
-        </p>
+    <div className={change?str:str1}>
+      <motion.div initial={{x:-100,opacity:0}} animate={{x:0,opacity:1}} className="text-9xl">Hello</motion.div>
+      <motion.div initial={{x:-100,opacity:0}} animate={{x:0,opacity:1}} transition={{delay:0.2}} className="text-9xl">world</motion.div>
+      <button onClick={()=>{
+        setChange(p=>!p)
+      }}>Change</button>
       </div>
-    </>
+    
   );
 }
 
